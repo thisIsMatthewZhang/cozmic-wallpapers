f9a55f96c86ab1deef2e2654a0228ffa6e258683
Matthew Zhang <zhangmatt22@gmail.com>
2026-05-14 17:28:13 -0500

    added Carousel component code

--- src/components/AppCarousel.tsx
+++ src/components/AppCarousel.tsx
@@ -1,15 +1,48 @@
-import Carousel, {ICarouselInstance, Pagination} from 'react-native-reanimated-carousel';
-import { Dimensions, View } from 'react-native'; 
+import Carousel, { ICarouselInstance, Pagination } from 'react-native-reanimated-carousel';
+import { Dimensions, View, Image, ImageSourcePropType } from 'react-native'; 
+import { useRef } from 'react';
+import { useSharedValue } from 'react-native-reanimated';
 
-type CarouselData<T> = {
-    data: T[]
+type CarouselData = {
+    data: ImageSourcePropType[]
 };
 const width = Dimensions.get("window").width;
 
-export default function AppCarousel<T>({ data }: Readonly<CarouselData<T>>) {
+export default function AppCarousel({ data }: Readonly<CarouselData>) {
+    const ref = useRef<ICarouselInstance | null>(null);
+    const progress = useSharedValue<number>(0);
+
+    const onPressPagination = (index: number) => {
+        ref.current?.scrollTo({
+            /**
+             * Calculate the difference between the current index and the target index
+             * to ensure that the carousel scrolls to the nearest index
+             */
+            count: index - progress.value,
+            animated: true,
+        });
+    };
+
+
     return (
-        <View>
-            <Carousel data={data} renderItem={({item}) => (<></>)} width={width} height={width / 2}/>
+        <View style={{ flex: 1 }}>
+            <Carousel 
+                data={data} 
+                renderItem={({item}) => (
+                <View style={{ flex: 1, borderWidth: 1, justifyContent: 'center' }}>
+                    <Image source={item} width={100} height={100}/>
+                </View>
+            )} 
+                width={width}
+                height={width / 2}
+            />
+            <Pagination.Basic 
+                progress={progress} 
+                data={data}
+                dotStyle={{ backgroundColor: "rgba(0,0,0,0.2)", borderRadius: 50 }}
+                containerStyle={{ gap: 5, marginTop: 10 }}
+                onPress={onPressPagination}
+            />
         </View>
     );
 }
